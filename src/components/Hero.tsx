c2fa6e5f8ca8b9ca68d34e5e4efef4fd8f25df1e
roxyle <ylenia.rossi@outlook.com>
2024-11-27 21:17:29 +0100

    I messed up the commits

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import CardIntro from './Cards/CardIntro'
-import { TextContent } from '@/constants/types'
 import { cardIntroTxt } from '@/constants/data'
 import ContPresentazione from './Containers/ContPresentazione'
 
@@ -10,10 +8,10 @@ const Hero = () => {
   return (
 
     
-      <div className="flex flex-col items-center justify-center flex-wrap p-10 bg-hero-pattern bg-cover"> 
+      <div className="flex flex-col items-center justify-center flex-wrap p-10 bg-hero-pattern"> 
         <div className='flex flex-row flex-wrap items-center justify-center'>
-          <h1 className="text-black text-center text-4xl font-bold">
-            Farti tornare in forma <br/>è la nostra missione
+          <h1 className="title text-center text-4xl font-bold">
+            Farti tornare in forma è la nostra missione
           </h1> 
 
           <div>
