b93952b8c2401e890d284e698ed78b1d24b4c810
roxyle <ylenia.rossi@outlook.com>
2024-11-29 12:08:56 +0100

    sistemato footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,20 +9,11 @@ const Footer = () => {
   return (
     <>
       <footer className='flex flex-row flex-wrap justify-center items-start 
-                        bg-sky-500 md:items-center md:justify-between
+                        bg-sky-500 md:justify-between 
                         overflow-auto text-white'>
 
-          <div className='flex flex-col items-center justify-center p-5'>
-            <h2 className='font-bold'>SEDE E CONTATTI</h2>
-            <ul className='flex flex-col justify-center items-center '>
-              <li>{ourContact.address}</li>
-              <li>{ourContact.email}</li>
-              <li>{ourContact.telephone}</li>
-            </ul>
-          </div>
-
-          <div className='flex flex-col items-center justify-start'>
-            <h2 className='font-bold md:pb-6'>ORARI DI APERTURA</h2>
+          <div className='flex flex-col items-center justify-start p-5 md:grow'>
+            <h2 className='font-bold md:px-6'>ORARI DI APERTURA</h2>
               <div className=''>
                 <p className='gap-3 flex flex-row flex-wrap items-end justify-between'>
                   <span>LUN - SAB</span> <span>8:30-20:00</span>
@@ -33,7 +24,16 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className='flex flex-col items-center justify-center py-5'>
+          <div className='flex flex-col items-center justify-center py-5 sm:pt-5 md:grow'>
+            <h2 className='font-bold '>SEDE E CONTATTI</h2>
+            <ul className='flex flex-col justify-center items-center '>
+              <li>{ourContact.address}</li>
+              <li>{ourContact.email}</li>
+              <li>{ourContact.telephone}</li>
+            </ul>
+          </div>
+
+          <div className='flex flex-col items-center justify-center sm:py-4 md:grow'>
               <BtnLogo/>
               <p>P.I. 04432730614</p>
             </div>
