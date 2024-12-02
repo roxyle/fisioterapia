a34468b1ccc7e1b9914aa7a56e0047822745754c
roxyle <ylenia.rossi@outlook.com>
2024-12-02 18:43:05 +0100

    Fixed some formatting

--- src/components/Containers/ContTxtImage.tsx
+++ src/components/Containers/ContTxtImage.tsx
@@ -19,17 +19,18 @@ const ContTxtImage = (dettagliTerapia: MyProps) => {
           <Image src='/Fisiok4.jpg' alt={'terapie strumentali'}
               width={700} height={600}
               className='p-5'/> 
-          <div className='flex flex-col items-center justify-center '>
 
-              <p className='text-black flex flex-col flex-wrap items-center justify-center text-center text-pretty p-5'>
+          <div className='text-black flex flex-col items-center justify-center '>
+
+              <p className='text-center text-pretty p-5'>
               L'aggiornamento costante alle novità del settore rende lo studio di fisioterapia di Caserta un centro capace 
-              di assistere ogni esigenza dei pazienti con terapie all'avanguardia, come quelle strumentali. Tra queste troviamo:
+              di assistere ogni esigenza dei pazienti con terapie all'avanguardia, come quelle strumentali. Tra queste troviamo:</p>
 
-              <ul className='list-disc py-5'>
+              <ul className='list-disc'>
                       {dettagliTerapia.elenco.map((e) => ( <li key={e.id}>{e.nomeTerapia}</li> ))}
               </ul>
 
-              Il team dello studio Fisioterapia Caserta è pronto ad assisterti anche con tecarpeterapia, magnetoterapia, 
+              <p className='text-center text-pretty p-5'>Il team dello studio Fisioterapia Caserta è pronto ad assisterti anche con tecarpeterapia, magnetoterapia, 
               terapia a onde d'urto e laserterapia a domicilio.
               </p>
           </div>
