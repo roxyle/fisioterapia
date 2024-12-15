7e2d5c4c19b272a4db8d20c64217a5d16eacd073
roxyle <ylenia.rossi@outlook.com>
2024-12-15 20:52:28 +0100

    Recupero dei file Cards.tsx e Containers.tsx dal commit 1e0810cde4d26eb651d7e5dbc8f7eb0aabc53dba

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,13 +8,13 @@ const Hero = () => {
   return (
 
     
-      <div className="flex flex-col items-center justify-center flex-wrap p-10 bg-hero-pattern"> 
-        <div className='flex flex-row flex-wrap items-center justify-center'>
+      <div className="bg-hero-pattern1 bg-no-repeat bg-cover"> 
+        <div className='flex flex-col items-center justify-center p-5'>
           <h1 className="title text-center text-4xl font-bold">
             Farti tornare in forma è la nostra missione
           </h1> 
 
-          <div>
+          
             <ContPresentazione cardIntroTxt={cardIntroTxt}/>
           </div>
         </div>
