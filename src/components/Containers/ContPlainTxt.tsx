52295f787a239ca057af3607ecb7fff0e1a5b9d9
roxyle <ylenia.rossi@outlook.com>
2024-12-15 22:36:17 +0100

    modificato sezione plaintext

--- src/components/Containers/ContPlainTxt.tsx
+++ src/components/Containers/ContPlainTxt.tsx
@@ -13,7 +13,7 @@ let location = usePathname()
 let simplePar = prop.txt.find((e) => e.id==location);
   return (
     <div className='bg-gradient-to-t from-white to-90% rounded-xl pt-5'>
-        <div className='flex flex-col items-center justify-center rounded-xl bg-white bg-opacity-50'>
+        <div className='flex flex-col items-center justify-center rounded-t-xl bg-white bg-opacity-50'>
             <h2 className='font-bold text-center text-pretty text-4xl title'>{simplePar?.titolo}</h2>
             <p className='text-black flex flex-row flex-wrap items-center justify-center text-center text-pretty p-5'>
             {simplePar?.testo}
