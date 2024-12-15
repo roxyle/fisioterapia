ab4fa1e376b97d685a7e8cf750f998adf856e58c
roxyle <ylenia.rossi@outlook.com>
2024-12-15 22:42:37 +0100

    modificato anche il contcardservizi

--- src/components/Containers/ContCardServizi.tsx
+++ src/components/Containers/ContCardServizi.tsx
@@ -9,8 +9,8 @@ type Props = {
 export default function ContCardServizi (props: Props) {
   return (
 
-        <div className='flex flex-col items-center bg-gradient-to-t from-white to-90% rounded-xl p-5'>
-          <h2 className='font-bold underline text-white text-4xl rounded-t-xl title'>Alcuni Dei Nostri Servizi </h2>
+        <div className='flex flex-col items-center bg-gradient-to-t from-white to-90% rounded-xl pb-5'>
+          <h2 className='font-bold text-white text-4xl rounded-t-xl title'>Alcuni Dei Nostri Servizi </h2>
 
              <div className='flex flex-col sm:flex-row flex-wrap justify-center'>
               {props.dettaglioServizi.map((tipologia)=>(
