1afd4ac4a6a22de7d884aa8adb4fa00d7470f236
roxyle <ylenia.rossi@outlook.com>
2024-12-18 14:41:11 +0100

    edited hero bg position for small devices

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
 
 
-      <div className="bg-hero-pattern1 bg-no-repeat bg-cover">
+      <div className="bg-hero-pattern1 bg-no-repeat bg-cover bg-custom bg-[35%_50%] md:bg-[center_top]">
         <div className='flex flex-col items-center justify-center p-5'>
 
             <ContPresentazione cardIntroTxt={cardIntroTxt}/>
