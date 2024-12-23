bd3494d0a98736c455093bc2d1a8e86f7ab52d6a
roxyle <ylenia.rossi@outlook.com>
2024-12-23 18:56:21 +0100

    test tra padding e gap

--- src/components/Containers/ContPresentazione.tsx
+++ src/components/Containers/ContPresentazione.tsx
@@ -11,7 +11,7 @@ export default function ContPresentazione(){
     <h1 className="title text-center text-4xl font-bold rounded-t-xl">
             Farti tornare in forma è la nostra missione
           </h1>
-    <div className='flex flex-col sm:grid sm:grid-cols-2 lg:flex lg:flex-row items-center justify-center gap-4'>
+    <div className='flex flex-col sm:grid sm:grid-cols-2 lg:flex lg:flex-row items-center justify-center'>
         {cardIntroTxt.map((txt)=>(
           <div className='px-5 py-5'>
             <CardIntro text={txt.text} key= {txt.id} id={txt.id}
