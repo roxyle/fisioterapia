b4b64f4bd5cfa85236f7a7e94e5a48d29b303ce3
roxyle <ylenia.rossi@outlook.com>
2024-12-23 18:34:48 +0100

    test3 con just-between

--- src/components/Containers/ContCallToActions.tsx
+++ src/components/Containers/ContCallToActions.tsx
@@ -11,7 +11,7 @@ export default function ContCallToActions(){
     <h1 className="title text-center text-4xl font-bold rounded-t-xl">
             Farti tornare in forma è la nostra missione
           </h1>
-    <div className='flex flex-col sm:flex-row flex-wrap items-center justify-center gap-3'>
+    <div className='flex flex-col sm:flex-row flex-wrap items-center justify-between gap-3'>
         {cardCTATxt.map((txt)=>(
 
             <CardIntro text={txt.text} key= {txt.id} id={txt.id}
