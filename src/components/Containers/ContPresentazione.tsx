2cda5757cc4e25b0668f88ba9997702c64791c3b
roxyle <ylenia.rossi@outlook.com>
2024-12-23 18:31:45 +0100

    modificato il responsive su conpresentazione e contcalltoaction per ipad(test2)

--- src/components/Containers/ContPresentazione.tsx
+++ src/components/Containers/ContPresentazione.tsx
@@ -11,7 +11,7 @@ export default function ContPresentazione(){
     <h1 className="title text-center text-4xl font-bold rounded-t-xl">
             Farti tornare in forma è la nostra missione
           </h1>
-    <div className='flex flex-col sm:flex-row sm:flex-wrap justify-center'>
+    <div className='flex flex-col sm:flex-row flex-wrap items-center justify-center gap-3'>
         {cardIntroTxt.map((txt)=>(
 
             <CardIntro text={txt.text} key= {txt.id} id={txt.id}
