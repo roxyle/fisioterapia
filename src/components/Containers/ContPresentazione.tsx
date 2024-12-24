1e839a2960e044d97c2a809f3e0d9471b68bcb06
roxyle <ylenia.rossi@outlook.com>
2024-12-24 18:16:15 +0100

    testing4

--- src/components/Containers/ContPresentazione.tsx
+++ src/components/Containers/ContPresentazione.tsx
@@ -8,13 +8,13 @@ export default function ContPresentazione(){
   return (
     <div className='bg-gradient-to-t from-white to-90% rounded-xl p-5 w-full' >
 
-    <h1 className="title text-center text-4xl font-bold rounded-t-xl text-red-600">
+    <h1 className="title text-center text-4xl font-bold rounded-t-xl">
             Farti tornare in forma è la nostra missione
           </h1>
     <div className='flex flex-col sm:grid sm:grid-cols-2 xl:flex xl:flex-row xl:flex-wrap items-center justify-center gap-3'>
         {cardIntroTxt.map((txt)=>(
           <div className=''>
-            <CardIntro text={txt.text} key= {txt.id} id={txt.id}
+            <CardIntro text={txt.text} key={txt.id} id={txt.id}
             immagine={`${txt.immagine}`} alt={'txt.alt'}
             bottone={`${txt.bottone}`} goToPage={`${txt.goToPage}`} />
           </div>
