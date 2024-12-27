6b6b4cd76cc325e35fe57e37593a9477f96317fe
roxyle <ylenia.rossi@outlook.com>
2024-12-27 22:52:55 +0100

    aggiunta sezione faq

--- src/components/Containers/ContFaq.tsx
+++ src/components/Containers/ContFaq.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
+import CardFaq from '../Cards/CardFaq'
+import { Faq } from '@/constants/data'
+
 
 const ContFaq = () => {
   return (
     
     <div className="bg-gradient-to-t from-white to-90% rounded-xl w-full py-3"> 
 
-        <h2 id='faq' className='rounded-t-xl font-bold text-4xl title'>Domande Frequenti</h2>
+        <h2 id='faq' className='rounded-t-xl font-bold text-4xl title' >Domande Frequenti</h2>
 
-        {/* <!--inizio seconda row contenente le accordion--> */}
         <div className="flex flex-col items-center justify-center
-        bg-white bg-opacity-10 rounded-b-xl py-5">
+        bg-white bg-opacity-10 rounded-b-xl p-4 gap-2">
             
+            {Faq.map((e)=>(
+            <CardFaq id={e.id} titolo={e.titolo} testo={e.testo}/>
+            ))
+            }
         </div>
     </div>
 
