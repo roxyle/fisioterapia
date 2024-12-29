bb95963e97c50cf7828938723de47ca24c04038a
roxyle <ylenia.rossi@outlook.com>
2024-12-29 12:22:30 +0100

    vedi commits precedenti, eliminato il bg white from-to dal contform

--- src/components/Containers/ContForm.tsx
+++ src/components/Containers/ContForm.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContForm = () => {
   return (
-<div className='bg-gradient-to-t from-white to-90% rounded-xl w-full py-3'>
+<div className='rounded-xl w-full py-3'>
     <h2 id='form' className='rounded-t-xl font-bold text-4xl title'> Compila il Form per essere ricontattato: </h2>    
     <div className='flex flex-col items-center justify-center
         bg-black bg-opacity-10 rounded-b-xl py-5'>
