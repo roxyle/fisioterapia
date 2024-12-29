ef453857a41e96b5608045b0b531f16fbba83c8b
roxyle <ylenia.rossi@outlook.com>
2024-12-29 18:45:30 +0100

    aggiunta validazione e fatto minor changes

--- src/components/Containers/ContCallToActions.tsx
+++ src/components/Containers/ContCallToActions.tsx
@@ -13,8 +13,8 @@ export default function ContCallToActions(){
           </h1>
     <div className='flex flex-col sm:grid sm:grid-cols-2 xl:flex xl:flex-row xl:flex-wrap items-center justify-center gap-3'>
         {cardCTATxt.map((txt)=>(
-          <div className=''>
-            <CardIntro text={txt.text} key={txt.id} id={txt.id}
+          <div key={`div+${txt.immagine}`}>
+            <CardIntro text={txt.text} key={`${txt.id}`} id={txt.id}
             immagine={`${txt.immagine}`} alt={'txt.alt'}
             bottone={`${txt.bottone}`} goToPage={`${txt.goToPage}`} />
           </div>
