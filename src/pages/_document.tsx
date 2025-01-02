e2aa94c9cfc0f0eb230f3675fb0a0035a40bb575
roxyle <ylenia.rossi@outlook.com>
2025-01-02 17:02:49 +0100

    aggiunti cookies

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en ">
       <Head />
+      <script id="Cookiebot" 
+      src="https://consent.cookiebot.com/uc.js" data-cbid="5cb7ed96-1c1b-4269-99ed-69b402f97049" data-blockingmode="auto" type="text/javascript">
+      </script>
       <body className="antialiased">
         <Main />
         <NextScript />
