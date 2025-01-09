1336893cec97a5a1e4dcb3f643782bc21ac26820
roxyle <ylenia.rossi@outlook.com>
2025-01-09 18:29:45 +0100

    modificato leggermente le altezze in cardpic e cardstaff

--- src/components/Cards/CardStaff.tsx
+++ src/components/Cards/CardStaff.tsx
@@ -15,7 +15,7 @@ const CardStaff = (dettaglioStaff: MyProps) => {
     <div className='flex flex-col p-5 inset-0 rounded-xl '>
 
         <Image className='rounded-t-xl object-cover
-        h-96 w-80
+        h-96 lg:h-[500px] w-80 lg:w-[350px]
         brightness-[.85] hover:filter-none
         border-t-2 border-x-2 border-sky-500
         ' key={dettaglioStaff.id} src={dettaglioStaff.immagine} alt={dettaglioStaff.nome} width={420} height={700}/>
