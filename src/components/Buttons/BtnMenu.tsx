fa69e808f17618c7668e291a5649f306356b7507
roxyle <ylenia.rossi@outlook.com>
2025-01-09 17:16:16 +0100

    aggiunto pathname nella navbar per la pagina attiva

--- src/components/Buttons/BtnMenu.tsx
+++ src/components/Buttons/BtnMenu.tsx
@@ -1,16 +1,52 @@
-"use Client"
-import Link from 'next/link'
-import React from 'react'
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation"; // Hook per ottenere il percorso attuale
 
 const BtnMenu = () => {
-  return (
-    <div className='md:py-5 flex flex-1 flex-row flex-wrap justify-evenly items-center text-white text-lg gap-3'>
-        <Link href='/' id='Home' className='hover:[text-decoration:underline_overline] hover:font-bold hover:text-lg underline-offset-4'>Home</Link>
-        <Link href='/Servizi' id='Servizi' className='hover:[text-decoration:underline_overline] hover:font-bold hover:text-lg underline-offset-4'>Servizi</Link>        
-        <Link href='/About' id="About" className='hover:[text-decoration:underline_overline] hover:font-bold hover:text-lg underline-offset-4'>Chi Siamo</Link>
-        <Link href='/Contatti' id='Contatti' className='hover:[text-decoration:underline_overline] hover:font-bold hover:text-lg underline-offset-4'>Contatti</Link>     
-    </div>
-  )
-}
+  const pathname = usePathname(); // Ottieni il percorso corrente
 
-export default BtnMenu
+  return (
+    <div className="md:py-5 flex flex-1 flex-row flex-wrap justify-evenly items-center text-white text-lg gap-3">
+      <Link
+        href="/"
+        id="Home"
+        className={`${
+          pathname === "/" ? "underline font-bold" : ""
+        } hover:underline hover:font-bold underline-offset-4`}
+      >
+        Home
+      </Link>
+      <Link
+        href="/Servizi"
+        id="Servizi"
+        className={`${
+          pathname === "/Servizi" ? "underline font-bold" : ""
+        } hover:underline hover:font-bold underline-offset-4`}
+      >
+        Servizi
+      </Link>
+      <Link
+        href="/About"
+        id="About"
+        className={`${
+          pathname === "/About" ? "underline font-bold" : ""
+        } hover:underline hover:font-bold underline-offset-4`}
+      >
+        Chi Siamo
+      </Link>
+      <Link
+        href="/Contatti"
+        id="Contatti"
+        className={`${
+          pathname === "/Contatti" ? "underline font-bold" : ""
+        } hover:underline hover:font-bold underline-offset-4`}
+      >
+        Contatti
+      </Link>
+    </div>
+  );
+};
+
+export default BtnMenu;
