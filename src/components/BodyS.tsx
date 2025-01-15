1bc6a2fcdaba50af2989fc3abd7fd557cb755a3a
roxyle <ylenia.rossi@outlook.com>
2025-01-15 19:19:42 +0100

    aggiunto head generale in document per home e meta basilari e aggiunto head specifici nelle altre pagine body

--- src/components/BodyS.tsx
+++ src/components/BodyS.tsx
@@ -3,13 +3,23 @@ import { DescServizi, Paragrafi, TerapieOrtopediche, TerapieStrumentali } from "
 import ContPlainTxt from './Containers/ContPlainTxt';
 import ContTxtImage from './Containers/ContTxtImage';
 import ContTxtImage2 from './Containers/ContTxtImage2';
+import Head from 'next/head';
 
 
 const BodyS = () => {
 
 
   return (
-
+    <>
+      <Head>
+        <title>Servizi di Fisioterapia a Caserta</title>
+        <meta
+          name="description"
+          content="Scopri tutti i servizi offerti dal centro di fisioterapia Bruno: massaggi, rieducazione posturale e terapie avanzate."
+        />
+        <meta name="keywords" content="fisioterapia caserta, terapie fisiche, massaggi caserta" />
+      </Head>
+      
     <div className='bg-terapie-pattern bg-cover'>
       <div className="p-5 flex flex-col items-center justify-center flex-wrap gap-5">
 
@@ -20,6 +30,7 @@ const BodyS = () => {
           <ContTxtImage elenco={TerapieStrumentali}/>
       </div>
     </div>
+    </>
   )
 }
 
