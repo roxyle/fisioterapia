440985f4f046cab29a8eb957ab1a39f2c26b0d74
roxyle <ylenia.rossi@outlook.com>
2025-01-15 19:19:42 +0100

    aggiunto head generale in document per home e meta basilari e aggiunto head specifici nelle altre pagine body

--- src/components/BodyC.tsx
+++ src/components/BodyC.tsx
@@ -5,19 +5,31 @@ import HeroContatti from './HeroContatti'
 import ContMap from './Containers/ContMap'
 import ContForm from './Containers/ContForm'
 import ContFaq from './Containers/ContFaq'
+import Head from 'next/head'
 
 const BodyC = () => {
   return (
-    <div className='bg-terapie-pattern bg-cover'>
-      <div className="p-2 flex flex-col items-center justify-center flex-wrap gap-2">
-        <ContPlainTxt txt={Paragrafi}/>
-        <ContForm/>
-        <ContMap/>
-        <ContFaq/>
+
+    <>
+    <Head>
+      <title>Contatta lo Studio Fisioterapia a Caserta</title>
+      <meta
+        name="description"
+        content="Chiama il centro di fisioterapia Bruno: potrai chiedere un consulto con il nostro Medico Chirurgo Ortopedico e prenota le migliori terapie."
+      />
+      <meta name="keywords" content="fisioterapista caserta, consulta il medico, prenota visite di massoterapia, ortopedia, tecar, laserterapia, magnetoterapia, caserta" />
+    </Head>
+      <div className='bg-terapie-pattern bg-cover'>
+        <div className="p-2 flex flex-col items-center justify-center flex-wrap gap-2">
+          <ContPlainTxt txt={Paragrafi}/>
+          <ContForm/>
+          <ContMap/>
+          <ContFaq/>
 
 
+        </div>
       </div>
-    </div>
+      </>
   )
 }
 export default BodyC
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang="en ">
       <Head />
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="author" content="Fisioterapia Bruno" />
+      <meta name="robots" content="index, follow" />
+      <link rel="icon" href="/favicon.ico" />
       <script id="Cookiebot" 
       src="https://consent.cookiebot.com/uc.js" data-cbid="5cb7ed96-1c1b-4269-99ed-69b402f97049" data-blockingmode="auto" type="text/javascript">
       </script>
