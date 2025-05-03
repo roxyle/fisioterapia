d4143f9261c1aecff34b8fd8b11c06e878252a71
roxyle <ylenia.rossi@outlook.com>
2025-05-03 16:15:41 +0200

    edited bg fixed on sm devices and local on md and lg

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
 
 
-    <div className="bg-hero-pattern2 bg-fixed md:bg-local lg:bg-hero-pattern1 bg-no-repeat bg-cover md:bg-[center_top]">
+    <div className="bg-hero-pattern2 bg-fixed md:bg-local lg:bg-hero-pattern1 bg-no-repeat bg-cover md:bg-[center_center]">
         <div className='flex flex-col items-center justify-center p-5'>
 
             <ContPresentazione/>
