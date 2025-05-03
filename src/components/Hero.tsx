5ac95a950dc1e8b5ee71221c3c7e011a3f81206b
roxyle <ylenia.rossi@outlook.com>
2025-05-03 15:28:31 +0200

    added empty tag inside hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,15 +5,15 @@ const Hero = () => {
 
   return (
 
-
-      <div className="bg-hero-pattern2 bg-fixed md:bg-hero-pattern1 bg-no-repeat bg-contain md:bg-cover bg-[center_center]">
+<>
+      <div className="bg-hero-pattern2 bg-fixed lg:bg-hero-pattern1 bg-no-repeat bg-contain lg:bg-cover bg-[center_center]">
         <div className='flex flex-col items-center justify-center p-5'>
 
             <ContPresentazione/>
           </div>
 
       </div>
-
+</>
   )
 }
 
