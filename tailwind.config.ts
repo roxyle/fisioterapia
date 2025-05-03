0dfee58a84666f97f8b8dbd7cc8fd27eb7449521
roxyle <ylenia.rossi@outlook.com>
2025-05-03 14:08:05 +0200

    edited tailwind config - theme - backgroundImage - hero-pattern1 to gif

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        'hero-pattern1': "url('/fisio6.mp4')",
+        'hero-pattern1': "url('/fisio6.gif')",
         'terapie-pattern':"url('/Fisio1921.jpg')",
         'terapie-strum-pattern':"url('/Fisiok6.jpg')",
 
